3fc5ac9cdf90c225660e9ab9bb5c2036c48356f8
tanyaowei <yaowei@tr8.io>
2022-09-23 17:26:50 +0800

    [ESD-589] update CLI exec file

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ```console
 foo@bar:~$ gattai run default ./env.gattai.yaml
 
-foo@bar:~$ gattai run <namespace> ./env.gattai.yaml
+foo@bar:~$ gattai run * mysql_password ./env.gattai.yaml
 
 foo@bar:~$ gattai run staging ./env.gattai.yaml --destination ./remote-dev-values.yaml
 
--- main.go
+++ main.go
@@ -4,31 +4,60 @@ import (
 	"os"
 	"fmt"
 	"log"
+	//"net/url"
 	"io/ioutil"
+	//"encoding/json"
 	"gopkg.in/yaml.v2"
 	"github.com/tr8team/gattai/src/gattai-core"
 	"text/template"
 )
 
+type Arguments map[string]interface{}
+
+type Target struct {
+	Exec string `yaml:"exec"`
+	Args Arguments `yaml:"args"`
+}
+
 type GattaiFile struct {
     Version string `yaml:"version"`
     TempFolder string `yaml:"temp_folder"`
 	EnforceTargets map[string][]string `yaml:"enforce_targets"`
-	Repos map[string]interface{} `yaml:"repos"`
-	Targets map[string]interface{} `yaml:"targets"`
+	Repos map[string]map[string]string `yaml:"repos"`
+	Targets map[string]map[string]Target `yaml:"targets"`
 }
 
-func tpl_fetch(target interface{}) string {
-	switch v := target.(type) {
-	case int:
-		fmt.Printf("Twice %v is %v\n", v, v*2)
-	case string:
-		fmt.Printf("%q is %v bytes long\n", v, len(v))
-	default:
-		fmt.Printf("I don't know about type %T!\n", v)
-	}
-	return "yes"
+type CLIFile struct {
+	Version string `yaml:"version"`
+
 }
+
+func tpl_fetch(lookup map[string]string) func(target Target) string {
+	return func(target Target) string {
+		yamlKey, err := yaml.Marshal(target)
+		if err != nil {
+			panic(err)
+		}
+
+		result, ok := lookup[string(yamlKey)]
+		if !ok {
+			result = target.Exec;
+			//switch v := target.Args.(type) {
+			//case map[interface {}]interface{}:
+			//	if exec, ok := v["exec"]; ok {
+			//		result = string(exec)
+			//	}
+			//default:
+			//	err := fmt.Sprintf("fetch do not support type %T!\n", v)
+			//	panic(err)
+			//}
+
+		}
+		return result
+	}
+}
+
+
 
 func main() {
 	argsWithoutProg := os.Args[1:]
@@ -37,20 +66,9 @@ func main() {
 
 	print.PrintHello()
 
-	temp, err := template.New("./env.gattai.yaml").Funcs(template.FuncMap{
-		"fetch": tpl_fetch,
-	  }).ParseFiles("./env.gattai.yaml")
-	  if err != nil {
-		panic(err)
-	  }
-    // Capture any error
-    if err != nil {
-        log.Fatalln(err)
-    }
-
 	var gattai_file GattaiFile
 
-	yamlFile, err := ioutil.ReadFile("./env.gattai.yaml")
+	yamlFile, err := ioutil.ReadFile("env.gattai.yaml")
     if err != nil {
         log.Printf("yamlFile.Get err   #%v ", err)
     }
@@ -58,6 +76,31 @@ func main() {
     if err != nil {
         log.Fatalf("Unmarshal: %v", err)
     }
+
+	lookUpMap := make(map[string]string)
+	//for key, val := gattai_file.Repos {
+	//	switch repo, ok := val["repo"];  ok  {
+	//	case url.ParseRequestURI(repo):
+	//		u, err :=
+	//		if err != nil {
+	//		   panic(err)
+	//		}
+	//	}
+	//}
+
+	tmpl, err := template.New("env.gattai.yaml").Funcs(template.FuncMap{
+		"fetch": tpl_fetch(lookUpMap),
+	}).ParseFiles("env.gattai.yaml")
+	if err != nil {
+		panic(err)
+	}
+    // Capture any error
+    if err != nil {
+        log.Fatalln(err)
+    }
+
     // Print out the template to std
-    temp.Execute(os.Stdout, gattai_file.Targets)
+	if err := tmpl.Execute(os.Stdout, gattai_file); err != nil {
+		fmt.Println(err)
+	}
 }
